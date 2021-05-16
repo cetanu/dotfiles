cfc90094b3ae8a1764dd182c7b7d586f5eaef0d0
Vasilios Syrakis <vsyrakis@atlassian.com>
2021-05-16 23:48:46 +1000

    make install script overwrite configs

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env fish
 mkdir -p ~/.config/nvim
-ln init.vim ~/.config/nvim/init.vim 2>/dev/null; or echo "nvim config installed"
+ln -f init.vim ~/.config/nvim/init.vim 2>/dev/null; and echo "nvim config installed"
 
 mkdir -p ~/.config/fish
-ln config.fish ~/.config/fish/config.fish 2>/dev/null; or echo "fish config installed"
+ln -f config.fish ~/.config/fish/config.fish 2>/dev/null; and echo "fish config installed"
 
 mkdir -p ~/.config/tmux
-ln tmux.conf ~/.config/tmux/.tmux.conf 2>/dev/null; or echo "tmux config installed"
+ln -f tmux.conf ~/.config/tmux/.tmux.conf 2>/dev/null; and echo "tmux config installed"
 
 mkdir -p ~/.config/alacritty
-ln alacritty.yml ~/.config/alacritty/alacritty.yml 2>/dev/null; or echo "alacritty config installed"
+ln -f alacritty.yml ~/.config/alacritty/alacritty.yml 2>/dev/null; and echo "alacritty config installed"
 
 mkdir -p ~/.config/starship
-ln starship.toml ~/.config/starship/starship.toml 2>/dev/null; or echo "starship config installed"
+ln -f starship.toml ~/.config/starship/starship.toml 2>/dev/null; and echo "starship config installed"
