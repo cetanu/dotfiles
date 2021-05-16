168e4c1ee145b15ee36a4b64596aa056fd44dfc0
Vasilios Syrakis <vsyrakis@atlassian.com>
2021-05-16 22:51:25 +1000

    updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-mkdir -p ~/.config/nvim/init.vim
-ln -s init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
+ln init.vim ~/.config/nvim/init.vim
 
+mkdir -p ~/.config/fish
+ln config.fish ~/.config/fish/config.fish
+
+mkdir -p ~/.config/tmux
+ln tmux.conf ~/.config/tmux/.tmux.conf
