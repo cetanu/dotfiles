ed10c95b16882c7fdbe7749de1c7fc4bbc1a37cb
Vasilios Syrakis <vsyrakis@atlassian.com>
2021-06-10 23:32:29 +1000

    Moved config into lua files using nvim_exec

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,21 @@
--- Load my old configuration while it's being migrated
-vim.cmd('source ~/.config/nvim/old_config.vim')
-
-
+require("plugins")
 require("options")
 require("themes")
 require("lsp")
 require("lua_lsp")
 require("treesitter")
 require("status_line")
+require("quickscope")
 require("keybindings")
+
+-- Misc stuff
+vim.api.nvim_exec([[
+syntax on
+autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()
+autocmd BufWritePre *.py execute ":Black"
+
+set colorcolumn=80
+highlight ColorColumn ctermbg=80 guibg=lightgrey
+
+let g:sls_use_jinja_syntax = 1
+]], false)
---
+++ nvim/lua/quickscope.lua
@@ -0,0 +1,4 @@
+vim.g.qs_hightlight_on_keys = {'f', 'F', 't', 'T'}
+vim.g.qs_max_chrs=150
+vim.api.nvim_exec("highlight QuickScopePrimary guifg='#00C7DF' gui=underline ctermfg=155 cterm=underline", false)
+vim.api.nvim_exec("highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=81 cterm=underline", false)
