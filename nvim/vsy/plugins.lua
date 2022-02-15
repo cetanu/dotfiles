68cb8aa77d4ef71fd4df7156d7b5648439825268
Vasili Syrakis <vsyrakis@atlassian.com>
2022-02-16 03:05:04 +1100

    Installed fidget, neogit, and some others

--- nvim/vsy/plugins.lua
+++ nvim/vsy/plugins.lua
@@ -1,46 +1,62 @@
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
-  use 'tpope/vim-fugitive' -- Git commands in nvim
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use 'LinArcX/telescope-command-palette.nvim'
+
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
+
   -- Add git related info in the signs columns and popups
+  use 'TimUntersberger/neogit'
+  use 'tpope/vim-fugitive' -- Git commands in nvim
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'sindrets/diffview.nvim', requires =  { 'nvim-lua/plenary.nvim' } }
+
   -- Treesitter
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+
   -- Completion
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-nvim-lua'
   use 'saadparwaiz1/cmp_luasnip'
+
   -- Snippets plugin
   use 'L3MON4D3/LuaSnip'
+
   -- LSP / Language support
   use 'neovim/nvim-lspconfig'
   use {'cespare/vim-toml', branch = 'main'}
+  use 'j-hui/fidget.nvim' -- fancy LSP UI elements
+
   -- Theme
   use 'rebelot/kanagawa.nvim'
   use 'itchyny/lightline.vim' -- Fancier statusline
+
   -- Startup speed
   use 'lewis6991/impatient.nvim'
 end)
 
 
 -- Assorted plugins that don't need their own module
-require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
-  },
+vim.highlight.create('GitGutterAdd', {ctermbg=0, guifg='green'}, false)
+vim.highlight.create('GitGutterChange', {ctermbg=0, guifg='orange'}, false)
+vim.highlight.create('GitGutterDelete', {ctermbg=0, guifg='red'}, false)
+
+require('gitsigns').setup()
+
+local neogit = require("neogit")
+
+neogit.setup{
+    integrations = {
+        diffview = true
+    }
 }
 
 require('Comment').setup()
