2f61890a48a7ddd2d30d4805fa0ed914313e675d
Vasilios Syrakis <cetanu@gmail.com>
2026-06-20 20:02:56 +1000

    update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -9,8 +9,8 @@ if [[ ! -f $(which brew) ]]; then
 fi
 echo "Homebrew installed"
 
-# yazi
-brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font
+# yazi & general tools
+brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font luarocks tree-sitter tree-sitter-cli lazygit llvm gopls tectonic chafa
 
 # orbstack
 brew install docker docker-compose orbstack
@@ -82,6 +82,17 @@ echo "Neovim installed"
 # pyenv
 [[ -f $(which pyenv) ]] || brew install pyenv
 echo "Pyenv installed"
+
+# Install pyenv build dependencies
+if [[ $(uname -s) == "Linux" ]]; then
+    if [[ -f /etc/debian_version ]]; then
+        sudo apt-get update
+        sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    fi
+elif [[ $(uname -s) == "Darwin" ]]; then
+    brew install openssl readline sqlite3 xz zlib tcl-tk
+fi
+
 PY_VERSIONS=(
     3.11.0
     3.12.0
@@ -95,6 +106,7 @@ for ver in ${PY_VERSIONS[@]}; do
         pyenv global 3.12.0
         pyenv init - | source
         python -m pip install --upgrade pip pipx poetry uv
+        python -m pip install pynvim salt-lsp
         uv tool install ruff@latest
         uv tool install tiptop
         echo "Python ${ver} installed"
@@ -110,6 +122,13 @@ echo "Node.js and NPM installed"
 NODE_LSPS=(
     bash-language-server
     yaml-language-server
+    awk-language-server
+    dockerfile-language-server-nodejs
+    vscode-langservers-extracted
+    typescript-language-server
+    typescript
+    @mermaid-js/mermaid-cli
+    neovim
 )
 for lsp in ${NODE_LSPS[@]}; do
     INSTALLED=$(npm list -g | grep ${lsp})
